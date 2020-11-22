0869ccf03d874d62312e72fe72e00ab1b92f19cd
Jasper <j.vanes1@student.avans.nl>
2020-11-22 11:47:02 +0100

    [EDIT] changed a couple methods in Client
    added a createPutRequest() and createDeleteRequest(). Also made it so all the methods return a boolean.

--- app/src/main/java/com/example/philipshueweekopdracht/Client.java
+++ app/src/main/java/com/example/philipshueweekopdracht/Client.java
@@ -1,9 +1,14 @@
 package com.example.philipshueweekopdracht;
 
-import org.json.JSONArray;
+import android.graphics.Color;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -51,6 +56,17 @@ public class Client {
         return request;
     }
 
+    private Request createPutRequest(String url, String json) {
+        RequestBody requestBody = RequestBody.create(json, JSON);
+        Request request = new Request.Builder().url("https://" + this.ipAddress + "/api/" + this.username + url).put(requestBody).build();
+        return request;
+    }
+
+    private Request createDeleteRequest(String url) {
+        Request request = new Request.Builder().url("https://" + this.ipAddress + "/api/" + this.username + url).delete().build();
+        return request;
+    }
+
     private String createResponse(Request request) {
         String responseString = "";
         try {
@@ -62,11 +78,70 @@ public class Client {
         return responseString;
     }
 
-    public void turnLightOn(int id) {
-        createResponse(createPostRequest("/lights/" + id + "/state", "{\"on\":true}"));
+    private void createAsyncResponse(Request request) {
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                //TODO: return response.body().string();
+            }
+        });
     }
 
-    public void turnLightOff(int id) {
-        createResponse(createPostRequest("/lights/" + id + "/state", "{\"on\":false}"));
+    public boolean turnLampOn(int id) {
+        String response = createResponse(createPutRequest("/lights/" + id + "/state", "{\"on\":true}"));
+        return response.contains("success");
+    }
+
+    public boolean turnLampOff(int id) {
+        String response = createResponse(createPutRequest("/lights/" + id + "/state", "{\"on\":false}"));
+        return response.contains("success");
+    }
+
+    public boolean deleteLamp(int id) {
+        String response = createResponse(createDeleteRequest("/lights/" + id));
+        return response.contains("success");
+    }
+
+    public boolean setLampBrightness(int id, int brightness) {
+        String response = createResponse(createPutRequest("/lights/" + id + "/state", "{\"bri\":" + brightness + "}"));
+        return response.contains("success");
+    }
+
+    public boolean setLampSaturation(int id, int saturation) {
+        String response = createResponse(createPutRequest("/lights/" + id + "/state", "{\"sat\":" + saturation + "}"));
+        return response.contains("success");
+    }
+
+    public boolean setLampColor(int id, int r, int g, int b) {
+        float[] hsb = {};
+        Color.RGBToHSV(r, g, b, hsb);
+
+        String response = createResponse(createPutRequest("/lights/" + id + "/state", "{\n" +
+                "    \"hue\":" + hsb[0] + ",\n" +
+                "    \"sat\":" + hsb[1] + ",\n" +
+                "    \"bri\":" + hsb[2] + "\n" +
+                "}"));
+        return response.contains("success");
+    }
+
+    public boolean setLampName(int id, String name) {
+        String response = createResponse(createPutRequest("/lights/" + id, "{\"name\":\"" + name + "\"}"));
+        return response.contains("success");
+    }
+
+    public ArrayList<Lamp> getAllLamps() {
+        ArrayList<Lamp> lampList = new ArrayList<>();
+
+        String response = createResponse(createGetRequest("/lights"));
+
+        //TODO: convert the reponse to lamp objects and put them in the lampList
+
+        return lampList;
     }
 }
